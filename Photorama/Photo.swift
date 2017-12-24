c75b7f2f6d49f738916f2a9ea8f9f8556c07a338
Roberto García <garciamercado.r@gmail.com>
2017-12-24 17:40:40 -0300

    Init

---
+++ Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Roberto García on 24-12-17.
+//  Copyright © 2017 Roberto García. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title:String, photoID:String, remoteURL:URL, dateTaken:Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+}
---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,63 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Roberto García on 24-12-17.
+//  Copyright © 2017 Roberto García. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        store.fetchInterestingPhotos {
+            (PhotoResult) -> Void in
+            
+            switch PhotoResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
